dc4be1d81fdd08dcb86e7ab518945e4d8eb85a3e
PepeLui17 <jl_mj_17@hotmail.com>
2016-04-16 16:28:12 -0500

    Implementada clase ImageLoadTask para cargar imagen desde una Url

--- app/src/main/java/espol/fiec/edu/lego/TallerActivity.java
+++ app/src/main/java/espol/fiec/edu/lego/TallerActivity.java
@@ -1,5 +1,7 @@
 package espol.fiec.edu.lego;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +11,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import espol.fiec.edu.lego.asynctasks.ImageLoadTask;
 
 /**
  * Created by Usuario on 26/03/2016.
@@ -38,10 +47,12 @@ public class TallerActivity extends AppCompatActivity implements View.OnClickLis
 
         viewFlipperInstrucciones = (ViewFlipper) findViewById(R.id.vfInstrucciones);
 
-        for(int i=0;i<gallery_grid_Images.length;i++)
+        //for(int i=0;i<gallery_grid_Images.length;i++)
+        for(int i=1;i<4;i++)
         {
             //  This will create dynamic image view and add them to ViewFlipper
-            setFlipperImage(gallery_grid_Images[i]);
+            //setFlipperImage(gallery_grid_Images[i]);
+            setFlipperImage(i);
         }
 
         btnAnterior.setOnClickListener(this);
@@ -52,9 +63,31 @@ public class TallerActivity extends AppCompatActivity implements View.OnClickLis
     private void setFlipperImage(int res) {
         Log.i("Set Flipper Called", res + "");
         ImageView image = new ImageView(getApplicationContext());
-        System.out.println("res: "+ res);
-        image.setBackgroundResource(res);
+        //System.out.println("res: "+ res);
+        //image.setBackgroundResource(res);
+        //image.setImageBitmap(getBitmapFromURL("http://icons.iconarchive.com/icons/crountch/one-piece-jolly-roger/72/Luffys-flag-icon.png"));
+        new ImageLoadTask("http://www.corporacionsmartest.com/lego_images/0"+res+".jpg", image).execute();
         viewFlipperInstrucciones.addView(image);
+    }
+
+    public Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src",src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+
+            System.out.print("Bitmap: " +  myBitmap.toString());
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
     }
 
     @Override
---
+++ app/src/main/java/espol/fiec/edu/lego/asynctasks/ImageLoadTask.java
@@ -0,0 +1,55 @@
+package espol.fiec.edu.lego.asynctasks;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Usuario on 16/04/2016.
+ */
+public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+    private String url;
+    private ImageView imageView;
+
+    public ImageLoadTask(String url, ImageView imageView) {
+        this.url = url;
+        this.imageView = imageView;
+    }
+
+    @Override
+    protected Bitmap doInBackground(Void... params) {
+        try {
+            URL urlConnection = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlConnection
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            //Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            ///
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 2; // el factor de escala a minimizar la imagen, siempre es potencia de 2
+
+            Bitmap myBitmap = BitmapFactory.decodeStream(input, new Rect(0, 0, 0, 0), options);
+            ///
+            return myBitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        super.onPostExecute(result);
+        imageView.setImageBitmap(result);
+    }
+
+}
