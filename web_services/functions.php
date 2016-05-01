cea7f9b1c24c33aa264656caa835be7834f370ba
edmunozg@gmail.com <edmunozg@gmail.com>
2016-05-01 14:25:21 -0500

    Funciones web services bloques y category

--- web_services/functions.php
+++ web_services/functions.php
@@ -1,11 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 include_once "connection.php";
 
 /**
@@ -15,26 +8,28 @@ include_once "connection.php";
  * @param string $password
  * @return array
  */
-function login($email, $password){
+function login_user($email, $password){
     $connection = db_connect();
     $login = array();    
     $query = "SELECT * FROM User WHERE email = '$email';";
     $result = mysqli_query($connection,$query);    
     if (!$result){
         $login[] = array(
-            'idUser'	=> '',
-            'username'	=> '',
-            'email'		=> '',
-            'password'  => 'Error en consulta -> '. mysqli_error($connection));
+            'idUser'    => '',
+            'Name'      => '',
+            'Username'  => '',
+            'Email'     => '',
+            'Password'  => 'Error en consulta -> '. mysqli_error($connection));
     }
     else{
         while ($row = mysqli_fetch_array($result)){
             if ($row['Password'] == $password){
                 $login[] = array(
-                    'idUser'	=> $row['idUser'],
-                    'username'	=> $row['Username'],
-                    'email'		=> $row['Email'],
-                    'password'	=> $row['Password']
+                    'idUser'    => $row['idUser'],
+                    'Name'      => $row['Name'],
+                    'Username'  => $row['Username'],
+                    'Email'     => $row['Email'],
+                    'Password'  => $row['Password']
                 );
             }
         }
@@ -53,12 +48,12 @@ function login($email, $password){
  * @param string $password
  * @return string
  */
-function insert_user($name, $username, $email, $password){
+function insert_user($name, $email, $password){
     $connection = db_connect();
     $response = '';
-    $query = "INSERT INTO User(Name, Username, Email, Password) "
+    $query = "INSERT INTO User(Name, Email, Password) "
             ."VALUES "
-            ."('$name', '$username', '$email', '$password');";
+            ."('$name', '$email', '$password');";
     $result = mysqli_query($connection, $query);
     if (!$result){
         $response = 'ERROR : Insert fallido -> ' . mysqli_error($connection);
@@ -70,4 +65,52 @@ function insert_user($name, $username, $email, $password){
     return $response;
 }
 
+function get_categories(){
+    $connection = db_connect();
+    $response = array();
+    $query = "SELECT * FROM Category;";
+    $result = mysqli_query($connection,$query);
+    if (!$result){
+        $response[] = array(
+            'idCategory'    => '',
+            'Name'  => 'Error en consulta -> '. mysqli_error($connection));
+    }
+    else{
+        while ($row = mysqli_fetch_array($result)){
+            $response[] = array(
+                'idCategory'    => $row['idCategory'],
+                'Name'          => $row['Name']
+            );
+        }
+    }
+    mysqli_close($connection);
+    return $response;
+}
 
+function get_bloques(){
+    $connection = db_connect();
+    $response = array();
+    $query = "SELECT * FROM Bloque;";
+    $result = mysqli_query($connection,$query);
+    if (!$result){
+        $response[] = array(
+            'idBloque'              => '',
+            'Category_idCategory'   => '',
+            'Title'                 => '',
+            'Description'           => '',
+            'Image'                 => 'Error en consulta -> '. mysqli_error($connection));
+    }
+    else{
+        while ($row = mysqli_fetch_array($result)){
+            $response[] = array(
+                'idBloque'              => $row['idBloque'],
+                'Category_idCategory'   => $row['Category_idCategory'],
+                'Title'                 => $row['Title'],
+                'Description'           => $row['Description'],
+                'Image'                 => $row['Image']
+            );
+        }
+    }
+    mysqli_close($connection);
+    return $response;
+}
--- web_services/wsLegoMindstrom.php
+++ web_services/wsLegoMindstrom.php
@@ -1,17 +1,14 @@
 <?php
-
 require_once('lib/nusoap.php');
 require_once('functions.php');
-
 /**
  * Consultas en internet
  * @reference http://atobeto-eremita.blogspot.com/2010/06/como-devolver-arreglos-y-estructuras-de.html
  * @reference https://en.wikipedia.org/wiki/PHPDoc
  */
-
 $soapServer = new soap_server();
-$serviceName = 'LegoMindstormsEspol';
-$nameSpace = 'urn:LegoMindstormsEspol';
+$serviceName = 'HogarDeCristo';
+$nameSpace = 'urn:HogarDeCristo';
 $soapServer->configureWSDL($serviceName,$nameSpace);
 $soapServer->wsdl->schemaTargetNamespace = $nameSpace;
 
@@ -24,9 +21,10 @@ $soapServer->wsdl->addComplexType(
     '',
     array(
         'idUser'    => array('name' => 'idUser',    'type' => 'xsd:string'),
-        'username'  => array('name' => 'username',  'type' => 'xsd:string'),
-        'email'     => array('name' => 'email',     'type' => 'xsd:string'),
-        'password'  => array('name' => 'password',  'type' => 'xsd:string')));
+        'Name'      => array('name' => 'Name',      'type' => 'xsd:string'),
+        'Username'  => array('name' => 'Username',  'type' => 'xsd:string'),
+        'Email'     => array('name' => 'Email',     'type' => 'xsd:string'),
+        'Password'  => array('name' => 'Password',  'type' => 'xsd:string')));
 
 $soapServer->wsdl->addComplexType(
     'arrayLogin',
@@ -41,15 +39,15 @@ $soapServer->wsdl->addComplexType(
 
 $soapServer->register(
     //Name
-    'login',
+    'login_user', 
     //Input:Parameter
     array('email' => 'xsd:string','password' => 'xsd:string'),
     //Output:Return
     array('return' => 'tns:arrayLogin'),
     //NameSpace
-    'urn:LegoMindstormsEspol',
+    'urn:HogarDeCristo',
     //SoapAction
-    'urn:LegoMindstormsEspol#login',
+    'urn:HogarDeCristo#login_user',
     //Style
     'rpc',
     //Use
@@ -57,25 +55,112 @@ $soapServer->register(
     //Documentation
     'Login de usuario');
 
-///////////////////////////////////////////
+////////////////////////////////////////////////
 
 $soapServer->register(
     //Name
     'insert_user',
     //Input:Parameter
-    array('name' => 'xsd:string', 'username' => 'xsd:string', 'email' => 'xsd:string', 'password' => 'xsd:string'),
+    array('name' => 'xsd:string', 'email' => 'xsd:string', 'password' => 'xsd:string'),
     //Output:Return
     array('return' => 'xsd:string'),
     //NameSpace
-    'urn:LegoMindstormsEspol',
+    'urn:HogarDeCristo',
     //SoapAction
-    'urn:LegoMindstormsEspol#insert_user',
+    'urn:HogarDeCristo#insert_user',
     //Style
     'rpc',
     //Use
     'encoded',
     //Documentation
     'Método para insertar un nuevo usuario a la base de datos.');
+
+////////////////////////////////////////////////
+// Define the struct data type
+$soapServer->wsdl->addComplexType(
+//Name
+    'structCategory',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+        'idCategory'    => array('name' => 'idCategory', 'type' => 'xsd:string'),
+        'Name'          => array('name' => 'Name', 'type' => 'xsd:string')));
+
+$soapServer->wsdl->addComplexType(
+    'arrayCategory',
+    'complexType',
+    'array',
+    '',
+    'SOAP-ENC:Array',
+    array(),
+    array(array('ref' => 'SOAP-ENC:arrayType','wsdl:arrayType' => 'tns:structCategory[]')),
+    'tns:structCategory'
+);
+// Registering the method
+$soapServer->register(
+//Name
+    'get_categories',
+    //Input:Parameter
+    array(),
+    //Output:Return
+    array('return' => 'tns:arrayCategory'),
+    //NameSpace
+    'urn:HogarDeCristo',
+    //SoapAction
+    'urn:HogarDeCristo#get_categories',
+    //Style
+    'rpc',
+    //Use
+    'encoded',
+    //Documentation
+    'Lista de categorias.');
+
+////////////////////////////////////////////////
+
+$soapServer->wsdl->addComplexType(
+//Name
+    'structBloque',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+        'idBloque'              => array('name' => 'idBloque', 'type' => 'xsd:string'),
+        'Category_idCategory'   => array('name' => 'Category_idCategory', 'type' => 'xsd:string'),
+        'Title'                 => array('name' => 'Title', 'type' => 'xsd:string'),
+        'Description'           => array('name' => 'Description', 'type' => 'xsd:string'),
+        'Image'                 => array('name' => 'Image', 'type' => 'xsd:base64Binary')));
+
+$soapServer->wsdl->addComplexType(
+    'arrayBloque',
+    'complexType',
+    'array',
+    '',
+    'SOAP-ENC:Array',
+    array(),
+    array(array('ref' => 'SOAP-ENC:arrayType','wsdl:arrayType' => 'tns:structBloque[]')),
+    'tns:structBloque'
+);
+
+$soapServer->register(
+//Name
+    'get_bloques',
+    //Input:Parameter
+    array(),
+    //Output:Return
+    array('return' => 'tns:arrayBloque'),
+    //NameSpace
+    'urn:HogarDeCristo',
+    //SoapAction
+    'urn:HogarDeCristo#get_bloques',
+    //Style
+    'rpc',
+    //Use
+    'encoded',
+    //Documentation
+    'Lista de bloques.');
 
 
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';       
